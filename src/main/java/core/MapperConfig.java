b556407ae2ece430dc08b3d33754281800e07366
jakelizzI <jakelizzI@gmail.com>
2017-02-21 07:32:30 +0900

    first commit

---
+++ src/main/java/core/MapperConfig.java
@@ -0,0 +1,8 @@
+
+
+public abstract class MapperConfig{
+	
+	MapperConfig(String yamlFilePath){
+		ObjectMapper objectMapper = new ObjectMapper();
+	}
+}
---
+++ src/main/java/core/ObjectMapper..java
@@ -0,0 +1,5 @@
+
+
+public <T> static object map(Class<T> clazz){
+	
+}
